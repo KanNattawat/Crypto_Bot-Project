d67e75cea53f211ac9b82b045b10a147f5993256
KanNattawat <nattawat26907@gmail.com>
2022-11-15 15:42:59 +0700

    Modified

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # Crypto_Bot
+from sklearn import tree
+import imageio.v3 as iio
+
+im1 = iio.imread('melon1.png')
+im2 = iio.imread('melon2.png')
+
+image = [[im1], [im2]]
+info = ["Cartoon", "Real"]
+
+classifier = tree.DecisionTreeClassifier()
+classifier = classifier.fit(image, info)
+print(*classifier.predict([[iio.imread(input())]]))
